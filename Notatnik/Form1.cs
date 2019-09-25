35b2985dae448f6e2d96be311794a61121eb3997
Student <krzysztof.bogal@up.krakow.pl>
2019-09-25 03:35:24 +0200

    Notatnik v1

---
+++ Notatnik/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notatnik
+{
+    public partial class Form1 : Form
+    {
+        //constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Notatnik_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ądWydrukuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ustawieniaStronyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        #region Metoda Zamknij
+        private void zamknijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        #endregion
+    }
+}
